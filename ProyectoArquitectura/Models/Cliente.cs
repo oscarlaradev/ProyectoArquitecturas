cbd07927e326930bd0b9042e156ece2f46e73b71
Griselda Luna García <grisluga05@gmail.com>
2026-05-10 20:37:32 -0600

    Update Cliente.cs

--- ProyectoArquitectura/Models/Cliente.cs
+++ ProyectoArquitectura/Models/Cliente.cs
@@ -2,17 +2,14 @@ using ProyectoArquitectura.Exceptions;
 
 namespace ProyectoArquitectura.Models
 {
-    /// <summary>
+
     /// Clase Cliente que hereda de la clase abstracta Persona.
     /// Aquí se representa a alguien que adopta mascotas.
-    /// </summary>
     public class Cliente : Persona
     {
         public string TelefonoDeContacto { get; set; } = string.Empty;
 
-        /// <summary>
         /// Constructor de la clase Cliente con el uso de Excepciones Personalizadas.
-        /// </summary>
         public Cliente(string nombre, int edad, string telefono)
         {
             // Validamos reglas y lanzamos excepciones personalizadas si no se cumplen
@@ -32,17 +29,13 @@ namespace ProyectoArquitectura.Models
             this.TelefonoDeContacto = telefono;
         }
 
-        /// <summary>
         /// Método abstracto de la clase padre (Persona).
-        /// </summary>
         public override string ObtenerRol()
         {
             return "Adoptante / Cliente";
         }
         
-        /// <summary>
         /// Sobreescribimos el método ToString para añadir información exclusiva del cliente.
-        /// </summary>
         public override string ToString()
         {
             // Usamos este return para aprovechar el código escrito en la clase padre
