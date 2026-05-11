babdaa776952655b1db4bbdb92fed0d986b1f529
Griselda Luna García <grisluga05@gmail.com>
2026-05-10 19:35:07 -0600

    Update Program.cs

--- ProyectoArquitectura/Program.cs
+++ ProyectoArquitectura/Program.cs
@@ -3,9 +3,7 @@ using ProyectoArquitectura.Runners;
 
 namespace ProyectoArquitectura
 {
-    /// <summary>
     /// Punto de entrada del sistema
-    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
