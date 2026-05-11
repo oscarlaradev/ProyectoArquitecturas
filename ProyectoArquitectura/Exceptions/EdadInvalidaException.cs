89c6a460bcb3c511fb082459ab5aa4459e481121
Griselda Luna García <grisluga05@gmail.com>
2026-05-10 20:46:44 -0600

    Update EdadInvalidaException.cs

--- ProyectoArquitectura/Exceptions/EdadInvalidaException.cs
+++ ProyectoArquitectura/Exceptions/EdadInvalidaException.cs
@@ -1,13 +1,9 @@
 namespace ProyectoArquitectura.Exceptions;
-    /// <summary>
+    
     /// Excepción personalizada específica para cuando la edad de una mascota o persona es ilógica.
-    /// </summary>
     public class EdadInvalidaException : Exception
     {
-        /// <summary>
         /// Constructor que recibe el mensaje específico sobre la edad incorrecta.
-        /// </summary>
-        /// <param name="message">Mensaje del error.</param>
         public EdadInvalidaException(string message) : base(message)
         {
         }
